03277ca6960644565487fdec44a7a83d5b8b1e98
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-10-15 15:24:16 +0200

    commint first

---
+++ script.js
@@ -0,0 +1,12 @@
+//Scrivi un programma che stampi i numeri da 1 a 100
+// ma per multipli di 3 stampi “Fizz” al posto del numero 
+// per multipli di 5 stampi Buzz. 
+//Per i numeri che sono multipli di 3 e 5, stampa FizzBuzz.
+
+// stampiamo i numeri da 1 a 100
+for(let i=0; i<100; i++){
+    let Num= i+1 // uso variabile Num come la somma per ottenere output da1 invece di zero
+    console.log(Num)
+}
+
+// 
