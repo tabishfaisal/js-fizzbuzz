70e3f47094253bd0328594321ed6b641710c6df4
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-10-15 20:01:46 +0200

    added everything

--- script.js
+++ script.js
@@ -5,8 +5,14 @@
 
 // stampiamo i numeri da 1 a 100
 for(let i=0; i<100; i++){
-    let Num= i+1 // uso variabile Num come la somma per ottenere output da1 invece di zero
-    console.log(Num)
+    let num = i+1
+    if(num%3===0 && num%5===0){ // metto la condizione usando method
+        console.log("FizzBuzz");
+    } else if (num%3===0){ // faccio stampare Fizz al posto dei numeri multipli di 3
+        console.log("Fizz");
+    } else if(num%5===0){ // faccio stampare buzz al posto dei numeri multipli di 5
+        console.log("Buzz");
+    } else { // faccio stampare tutti i numeri naturali 
+        console.log(num);
+    }
 }
-
-// 
